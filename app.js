9fca910a97d1a68d74d3f60332cea7093acbe037
wisecio <alt.perturbancy@simplelogin.co>
2022-04-04 21:30:02 +0100

    final innit

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
+import {
+  presets,
+  queueWrite
+} from "https://cdn.skypack.dev/glitched-writer@2.0.29";
 
-   
+const phrases = [
+  "Hello there.",
+  "Yes, we knew you would come by at some point.",
+  "We would like to present you",
+  "our new baby.",
+  "wisec.io",
+  "Future is now",
+  "Standby."
+
+];
+
+
+
+queueWrite(
+  phrases,
+  ".text",
+  {
+    ...presets.neo,
+    letterize: true
+  },
+  2000,
+  10000
+);
+
+
+
+setTimeout(async () => {
+	const { string } = await Writer.play() // continue writing
+	console.log('Completed:', string) // will log after finished writing
+}, 2000)
+
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
  
-
-const text = document.querySelector('.text');
-
-const writer = GlitchedWriter.create(text);
-
-let phrases = [
-    "chapo",
-    "to",
-    "jebana",
-    "pizda",
-    
-];
-
-writer.queueWrite(phrases, 2000, true);
+ 
+  
+ 
