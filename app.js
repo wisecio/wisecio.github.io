9c4c54eb3e93f357faa079e6e7389440d619c507
wisecio <alt.perturbancy@simplelogin.co>
2022-04-04 19:27:20 +0100

    Co-authored-by: FakeChapo - cwel<FakeChapo@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-'use strict'
 
-const switcher = document.querySelector('.btn');
-
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme')
-
-    var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    }
-    else {
-        this.textContent = "Light";
-    }
-
-    console.log('current class name: ; + className')
    
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
  
-});
+
+const text = document.querySelector('.text');
+
+const writer = GlitchedWriter.create(text);
+
+let phrases = [
+    "chapo",
+    "to",
+    "jebana",
+    "pizda",
+    
+];
+
+writer.queueWrite(phrases, 2000, true);
